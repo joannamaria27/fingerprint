9c5e507c574c7b985858d82f80ff92241b7d98c9
joannamaria27 <joan.mk7@gmail.com>
2020-05-16 13:52:12 +0200

    start kmm

--- fingerprint/MainWindow.xaml.cs
+++ fingerprint/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Windows;
@@ -186,8 +187,181 @@ namespace fingerprint
         }
 
 
+        private void Szkieletyzacja(object sender, RoutedEventArgs e)
+        {
+            BitmapImage source = obrazek_2.Source as BitmapImage;
+            Bitmap b = BitmapImage2DBitmap(source);
+            SzkieletyzacjaKMM(b);
+        }
+        private void SzkieletyzacjaKMM(Bitmap b) //po binaryzacji
+        {
+            int[] listaCzworek = { 3, 6, 7, 12, 14, 15, 24, 28, 30, 48, 56, 60, 96, 112, 120, 129, 131, 135, 192, 193, 195, 224, 225, 240 };
+            int[,] maksaSprawdzajaca = { { 128, 64, 32 }, { 1, 0, 16 }, { 2, 4, 8 } };
+            int[] maskaWyciec = { 3, 5, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31, 48, 52, 53, 54, 55, 56, 60, 61, 62, 63, 65, 67, 69, 71, 77, 79, 80, 81, 83, 84, 85, 86, 87, 88, 89, 91, 92, 93, 94, 95, 97, 99, 101, 103, 109, 111, 112, 113, 115, 116, 117, 118, 119, 120, 121, 123, 124, 125, 126, 127, 131, 133, 135, 141, 143, 149, 151, 157, 159, 181, 183, 189, 191, 192, 193, 195, 197, 199, 205, 207, 208, 209, 211, 212, 213, 214, 215, 216, 217, 219, 220, 221, 222, 223, 224, 225, 227, 229, 231, 237, 239, 240, 241, 243, 244, 245, 246, 247, 248, 249, 251, 252, 253, 254, 255 };
+            List<int> wciecia = new List<int>(maskaWyciec);
+            int dlugosc = 1;
+            int[,] nowePixele = new int[b.Width, b.Height];
+            for (int x = dlugosc; x < b.Width - dlugosc; x++)
+            {
+                for (int y = dlugosc; y < b.Height - dlugosc; y++)
+                {
+                    Color koloryOb = b.GetPixel(x, y);
+                    if (koloryOb.R == 0) nowePixele[x, y] = 0;
+                    else nowePixele[x, y] = 1;
+                }
+            }
+            int[,] nowszePixele= { { 0 } };
+
+            while (nowePixele!= nowszePixele)
+            {
+                nowszePixele = nowePixele;
+
+                for (int x = dlugosc; x < b.Width - dlugosc; x++)
+                {
+                    for (int y = dlugosc; y < b.Height - dlugosc; y++)
+                    {
+                        if (nowePixele[x, y] == 1)
+                        {
+                            if (nowePixele[x + 1, y] == 0 || nowePixele[x, y + 1] == 0 || nowePixele[x, y - 1] == 0 || nowePixele[x - 1, y] == 0)
+                            {
+                                nowePixele[x, y] = 2;
+                            }
+                            if (nowePixele[x + 1, y + 1] == 0 || nowePixele[x - 1, y + 1] == 0 || nowePixele[x - 1, y - 1] == 0 || nowePixele[x + 1, y - 1] == 0)
+                            {
+                                nowePixele[x, y] = 3;
+                            }
+                        }
+                    }
+                }
+
+                for (int x = dlugosc; x < b.Width - dlugosc; x++)
+                {
+                    for (int y = dlugosc; y < b.Height - dlugosc; y++)
+                    {
+                        if (nowePixele[x, y] == 2)
+                        {
+                            for (int i = -1; i <= 1; i++)
+                            {
+                                for (int j = -1; j <= 1; j++)
+                                {
+                                    if (nowePixele[x + i, y + j] == 1 || nowePixele[x + i, y + j] == 2 || nowePixele[x + i, y + j] == 3)
+                                    {
+                                        nowePixele[x, y] = 0;
+                                        nowePixele[x, y] += maksaSprawdzajaca[1 + i, 1 + j];
 
 
+                                    }
+                                }
+                            }
+                            List<int> czworki = new List<int>(listaCzworek);
+                            if (czworki.Contains(nowePixele[x, y]))
+                                nowePixele[x, y] = 4;
+                            else
+                                nowePixele[x, y] = 2;
+                        }
+                    }
+                }
+
+                for (int x = dlugosc; x < b.Width - dlugosc; x++)
+                {
+                    for (int y = dlugosc; y < b.Height - dlugosc; y++)
+                    {
+                        if (nowePixele[x, y] == 4)
+                        {
+                            for (int i = -1; i <= 1; i++)
+                            {
+                                for (int j = -1; j <= 1; j++)
+                                {
+                                    if (nowePixele[x + i, y + j] == 1 || nowePixele[x + i, y + j] == 2 || nowePixele[x + i, y + j] == 3)
+                                    {
+                                        nowePixele[x, y] = 0;
+                                        nowePixele[x, y] += maksaSprawdzajaca[1 + i, 1 + j];
+
+
+                                    }
+                                }
+                            }
+                            if (wciecia.Contains(nowePixele[x, y]))
+                                nowePixele[x, y] = 0;
+                            else
+                                nowePixele[x, y] = 1;
+                        }
+
+                    }
+                }
+
+                for (int x = dlugosc; x < b.Width - dlugosc; x++)
+                {
+                    for (int y = dlugosc; y < b.Height - dlugosc; y++)
+                    {
+                        if (nowePixele[x, y] == 2)
+                        {
+                            for (int i = -1; i <= 1; i++)
+                            {
+                                for (int j = -1; j <= 1; j++)
+                                {
+                                    if (nowePixele[x + i, y + j] == 1 || nowePixele[x + i, y + j] == 2 || nowePixele[x + i, y + j] == 3)
+                                    {
+                                        nowePixele[x, y] = 0;
+                                        nowePixele[x, y] += maksaSprawdzajaca[1 + i, 1 + j];
+
+
+                                    }
+                                }
+                            }
+                            if (wciecia.Contains(nowePixele[x, y]))
+                                nowePixele[x, y] = 0;
+                            else
+                                nowePixele[x, y] = 1;
+                        }
+
+                    }
+                }
+
+                for (int x = dlugosc; x < b.Width - dlugosc; x++)
+                {
+                    for (int y = dlugosc; y < b.Height - dlugosc; y++)
+                    {
+                        if (nowePixele[x, y] == 3)
+                        {
+                            for (int i = -1; i <= 1; i++)
+                            {
+                                for (int j = -1; j <= 1; j++)
+                                {
+                                    if (nowePixele[x + i, y + j] == 1 || nowePixele[x + i, y + j] == 2 || nowePixele[x + i, y + j] == 3)
+                                    {
+                                        nowePixele[x, y] = 0;
+                                        nowePixele[x, y] += maksaSprawdzajaca[1 + i, 1 + j];
+
+
+                                    }
+                                }
+                            }
+                            if (wciecia.Contains(nowePixele[x, y]))
+                                nowePixele[x, y] = 0;
+                            else
+                                nowePixele[x, y] = 1;
+                        }
+
+                    }
+                }
+
+                
+
+
+            }
+
+            for (int x = dlugosc; x < b.Width - dlugosc; x++)
+            {
+                for (int y = dlugosc; y < b.Height - dlugosc; y++)
+                {
+                    if (nowePixele[x, y] == 1) nowePixele[x, y] = 255;
+                    b.SetPixel(x, y, Color.FromArgb(nowePixele[x, y], nowePixele[x, y], nowePixele[x, y]));
+
+                }
+            }
+
+
+        }
     }
 }
-
